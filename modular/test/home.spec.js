833372e55f22237c84d9797b4848db51df11525f
Sergei Garcia <sergei.garcia@ipointsystems.com>
2016-08-02 14:31:20 -0500

    minor tweaks

--- modular/test/home.spec.js
+++ modular/test/home.spec.js
@@ -1,19 +1,17 @@
 var expect = require('chai').expect,
 	HomeController = require('./../src/home/home.controller'),
-	mocks = {
-		todoFactory: require('./mocks/todo.factory')
-	};
+	todoFactoryMock = require('./mocks/todo.factory');
 
 describe('Home Controller:', function () {
 	var homeController = null,
 		todoFactory = null;
 
 	beforeEach(function () {
-		todoFactory = mocks.todoFactory();
+		todoFactory = todoFactoryMock();
 		homeController = new HomeController(todoFactory);
 	});
 
-	it('Todo should be stored', function () {
+	it('Should mark todo as saved', function () {
 		homeController.createTodo();
 		expect(todoFactory.todoStored).to.equal(true);
 	});
--- non-modular/test/home.spec.js
+++ non-modular/test/home.spec.js
@@ -1,8 +1,39 @@
-describe('home module tests', function() {
+var todoFactoryMock = {
+	todoStored: false,
+	lastTodo: null,
+	todos: [
+		{
+			id: new Date().getTime(),
+			content: 'Sample Todo',
+			date: new Date()
+		}
+	],
+	addTodo: function (todo) {
+		todoFactoryMock.lastTodo = todo;
+		todoFactoryMock.todoStored = true;
+		return true;
+	}
+};
+
+describe('Home Controller:', function () {
 	beforeEach(function () {
 		module('myApp');
 	});
-	it('should be true', function() {
-		expect(true).to.equal(true);
+
+	var $controller = null;
+	beforeEach(inject(function (_$controller_) {
+		$controller = _$controller_;
+	}));
+
+	var homeController = null;
+	beforeEach(function () {
+		homeController = $controller('homeController', {
+			todoFactory: todoFactoryMock
+		});
+	});
+
+	it('Should mark todo as saved', function () {
+		homeController.createTodo();
+		expect(todoFactoryMock.todoStored).to.equal(true);
 	});
 });
